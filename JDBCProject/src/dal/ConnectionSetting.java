30f002c2d47d34e558e8898906661bf3188fcf96
thaamserku <mailxitiz@gmail.com>
2022-07-09 16:29:32 -0400

    Added some clarifying comments.

--- JDBCProject/src/dal/ConnectionSetting.java
+++ JDBCProject/src/dal/ConnectionSetting.java
@@ -4,16 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/*-
+ * This class is all about using JDBC to establish connection 
+ * to MySQL database.
+ */
 public class ConnectionSetting {
 
+	/*-
+	 * Declare relevant constants;
+	 */
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbc";
 	private static final String USER = "root";
 	private static final String PASSWORD = "Kathmandu@1410";
 
-	// this constructor loads the driver
-	// no need to load driver since JDBC 4.0 and java 1.6 though.
-	// Just to demo how it can be done via constructor
+	/*-
+	 *  1. The following constructor loads the JDBC driver for MySQL.
+	 *  2. No need to explicitly load driver since JDBC 4.0 and java 1.6 though.
+	 *  	When the Connection is returned with getConnection, the driver is implicitly loaded first.
+	 *  3. Keep it as a demo as to how to do it via constructor.
+	 */
 	public ConnectionSetting() {
 		try {
 			Class.forName(DRIVER);
